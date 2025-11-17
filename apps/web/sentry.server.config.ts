e150cd33592b7432895a9c9e447bfe1181ab7626
Nilabh Jyoti Kalita <73031842+nilabh-astrophysics@users.noreply.github.com>
2025-11-17 21:45:36 +0530

    Delete apps/web/sentry.server.config.ts

--- apps/web/sentry.server.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 0.1,
-  environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV,
-});
